24adcd02624de62ef855e04cf3132d44bf63b14c
Lars <lars.eriksson.mail@gmail.com>
2022-03-13 16:36:15 +0100

    Added sms functionality from twilio.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import requests
+from twilio.rest import Client
 
 API_Key = "1916d150df13c5197c107e364a08863b"
-#MY_LAT = 59.2928
-#MY_LONG = 15.2151
+MY_LAT = 59.2928
+MY_LONG = 15.2151
 OWM_Endpoint = "http://api.openweathermap.org/data/2.5/onecall"
-MY_LAT = 43.36
-MY_LONG = 3.52
+
+account_sid =''
+auth_token = ''
+to_number = ''
+from_number = ''
 
 params = {
     "lat": MY_LAT,
@@ -15,23 +19,35 @@ params = {
     "exclude": "current,daily,minutely",
 }
 
-def umbrella():
+api = requests.get(OWM_Endpoint, params=params)
+api.raise_for_status()
 
-    api = requests.get(OWM_Endpoint, params=params)
-    api.raise_for_status()
+weather = api.json()
 
-    weather = api.json()
+hourly_weather = weather["hourly"]
+weather_slice = hourly_weather[:12]
 
-    hourly_weather = weather["hourly"]
-
-
-    for i in range(0, 12):
-        if int(hourly_weather[i]["weather"][0]["id"]) < 600:
-            return True
+will_rain = False
 
 
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 600:
+        will_rain = True
 
-print(umbrella())
+
+
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+                    .create(
+                         body="It will rain today.",
+                         from_='',
+                         to=''
+                     )
+
 
 
 
