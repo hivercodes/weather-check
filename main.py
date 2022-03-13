7ab8988eb55c6ea3af42f3eaa1c197992975f149
Lars <lars.eriksson.mail@gmail.com>
2022-03-13 15:58:11 +0100

    Parsed the json for rain during the next 12 hours.

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import requests
 
 API_Key = "1916d150df13c5197c107e364a08863b"
-MY_LAT = 59.2928
-MY_LONG = 15.2151
+#MY_LAT = 59.2928
+#MY_LONG = 15.2151
 OWM_Endpoint = "http://api.openweathermap.org/data/2.5/onecall"
+MY_LAT = 43.36
+MY_LONG = 3.52
 
 params = {
     "lat": MY_LAT,
     "lon":MY_LONG,
     "appid": API_Key,
-    "units": "metric"
+    "units": "metric",
+    "exclude": "current,daily,minutely",
 }
 
+def umbrella():
+
+    api = requests.get(OWM_Endpoint, params=params)
+    api.raise_for_status()
+
+    weather = api.json()
+
+    hourly_weather = weather["hourly"]
 
 
-api = requests.get(OWM_Endpoint, params=params)
+    for i in range(0, 12):
+        if int(hourly_weather[i]["weather"][0]["id"]) < 600:
+            return True
 
-weather = api.json()
 
-hourly_weather = weather["hourly"]
 
-print(hourly_weather)
+print(umbrella())
+
+
 
 
